194fd36202b25bc14884bf800acd7e16162b5ab1
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-12-03 23:50:10 +0000

    TP-62 : update LaundryDashboard component: adjust package title font size and maintain price styling

--- src/app/(dashboard)/services/page.tsx
+++ src/app/(dashboard)/services/page.tsx
@@ -211,8 +211,8 @@ export default function LaundryDashboard() {
                             ].map((pkg, index) => (
                                 <Card key={index} className={`overflow-hidden rounded-xl ${isDarkTheme ? 'bg-gray-800' : 'bg-blue-50'} shadow-md transition-transform transform hover:scale-105`}>
                                     <CardContent className={`p-6 text-left ${isDarkTheme ? 'text-gray-200' : 'text-gray-900'}`}>
-                                        <h3 className="font-semi-bold text-teal text-xl">{pkg.title}</h3>
-                                        <div className="text-2xl font-bold text-teal mt-1 block">{pkg.price}</div><br />
+                                        <h3 className="font-semi-bold text-teal text-sm">{pkg.title}</h3>
+                                        <div className="text-2xl  text-teal mt-1 block">{pkg.price}</div><br />
                                         <ul className="mb-6 space-y-2 text-xs text-slate-500">
                                             {pkg.features.map((feature, index) => (
                                                 <li key={index} className="flex items-center gap-2">
