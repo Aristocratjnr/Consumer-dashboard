9b3f33f209c99cea924218d0b4ada04d53187bd5
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-12-04 10:16:53 +0000

    TP-65 : update LaundryService component add new service page with service descriptions and selection options

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,12 @@ import TrackingPage from "./(dashboard)/tracking/page";
 import Dashboard from "./(dashboard)/home/page";
 import Page from "./LandingPage/page";
 import ServicesPage from "./(dashboard)/services/page";
+import LaundryService from "./(dashboard)/LaundryService/page";
 
 const page = () => {
   return (
     <div className="min-h-screen bg-white">
-      <ServicesPage/>
+      <LaundryService/>
     </div>
   );
 };
