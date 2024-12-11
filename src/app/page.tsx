1e6bdfd242fb46c750481552b3532ff530ed1f8c
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-12-11 22:41:44 +0000

    feat: add Radix UI Select component and integrate into BookingForm; update package.json dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,12 @@ import Dashboard from "./(dashboard)/home/page";
 import LandingPage from "./LandingPage/page"; 
 import SignIn from "./auth/SignIn/page";
 import ServicesPage from "./(dashboard)/services/page";
-import SearchUI from "../components/page";
+import BookingForm from "./(dashboard)/ConsumerServicesBoard/HomeService";
 
 const page = () => {
   return (
     <div className="min-h-screen bg-white">
-      <Dashboard/>
+      <BookingForm/>
     </div>
   );
 };
