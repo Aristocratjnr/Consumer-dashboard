4f286690e52bd62bd5c85a30828db3d4348f2e62
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-12-15 20:36:53 +0000

    feat: adjust padding in main section for improved layout in Dashboard

--- src/app/(dashboard)/home/page.tsx
+++ src/app/(dashboard)/home/page.tsx
@@ -260,7 +260,7 @@ export default function Dashboard() {
           </button>
         </aside>
 
-        <main className="flex-1 ml-64 mr-96 px-6 py-6">
+        <main className="flex-1 ml-64 mr-96 px-4 py-2">
           <div className="max-w-4xl">
             <div className="mb-8">
               <h1 className="text-4xl font-semi-bold">Welcome <span className="text-teal">Back!</span></h1>
