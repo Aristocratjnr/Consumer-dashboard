74bcc3a76237a120d80b0e93d275be157bccc654
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2025-01-17 07:40:04 +0000

    TP-87: added a hardcore auth logic  by adding user ID to session and protection middleware

--- src/lib/authOptions.ts
+++ src/lib/authOptions.ts
@@ -1,6 +1,17 @@
 import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+    };
+  }
+}
+
 
 export const authOptions: NextAuthOptions = {
     providers: [
@@ -10,4 +21,14 @@ export const authOptions: NextAuthOptions = {
         }),
       ],
       secret: process.env.NEXTAUTH_SECRET || "defaultFallbackSecret",
+      callbacks: {
+        async session({ session, token }) {
+          if (session.user) {
+            if (token.sub) {
+              session.user.id = token.sub; 
+            }
+          }
+          return session;
+        },
+      },
     };
---
+++ src/middleware.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { getToken } from "next-auth/jwt";
+
+// Middleware to protect specific routes
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+  // Redirect to sign-in if no token is found
+  if (!token) {
+    const signInUrl = new URL("/auth/SignIn", req.url);
+    return NextResponse.redirect(signInUrl);
+  }
+
+  // Proceed with the request if token exists
+  return NextResponse.next();
+}
+
+// Define the routes where the middleware should be applied
+export const config = {
+  matcher: ["/home/:path*"], // Protect /dashboard and its subroutes
+};
